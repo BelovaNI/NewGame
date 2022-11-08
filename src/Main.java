181dfc513045a1c634426093794ae2a35182ca5c
BelovaNI <belova.ni@inbox.ru>
2022-11-08 19:09:24 +0500

    Go

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static class GuessANumber {
+        public static void main (String [] args) {
+            int UnknownNumber, UserNumber, TrustCont = 0;
+            Scanner input = new Scanner (System.in);
+            System.out.println("Задача: Угадать число (от 0 до 100).");
+            UnknownNumber = (int) Math.floor(Math.random() * 100);
+            do {
+                TrustCont++;
+                System.out.println("Введите ваше число: ");
+                UserNumber = input.nextInt();
+                if (UserNumber > UnknownNumber)
+                    System.out.println("Не ожидал от тебя такого. Загаданное число меньше, брат.");
+                else if (UserNumber < UnknownNumber)
+                    System.out.println("Я сам в шоке, но загаданное число больше, брат");
+                else System.out.println("Ты угадал, братишка!");
+
+            } while (UserNumber != UnknownNumber);
+            System.out.println("Количество попыток: " +TrustCont);
+
+        }
+
     }
+
 }
